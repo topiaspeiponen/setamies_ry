7fa503defd9f4d6bde608de3a6270fb004f4306a
RoopeVaarama <roope.vaarama@gmail.com>
2019-12-04 14:38:43 +0200

    login

--- app.js
+++ app.js
@@ -2,13 +2,42 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const cors = require('cors');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
+const listing = require("./routes/listingRoute");
+app.use('/listings', listing);
+
+app.set('view-engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: "Kyle"});
+    });
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+});
+app.post('/login', (req, res) => {
+
+});
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+});
+
+app.post('/register', (req, res) => {
+
+});
+
+app.listen(port, () => console.log(`Project app listening on port ${port}!`));
+
+
+
+
+
+/*
 const passport = require('passport');
 const flash = require('connect-flash');
 
@@ -16,7 +45,7 @@ app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 //DEFINE ALL CONFIGS BEFORE ROUTES
-const listing = require("./routes/listingRoute");
+
 const login = require("./routes/loginRoutes");
 
 
@@ -40,7 +69,4 @@ app.use(flash());
 app.use('/login', login);
 
 require('./routes/loginRoutes')(app, passport);
-app.use(cors());
-app.use('/listings', listing);
-
-app.listen(port, () => console.log(`Project app listening on port ${port}!`));
+app.use(cors());*/
